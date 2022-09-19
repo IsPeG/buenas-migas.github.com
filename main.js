3f9a273430ee937dc2726c5839e9cb09183b9b00
IsPeG <igr13267@gmail.com>
2022-09-19 19:55:11 +0200

    Cambios final dia

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-ScrollReveal().reveal('.title', { delay: 200 });
-ScrollReveal().reveal('.image-title', { delay: 350 });
+ScrollReveal().reveal('.title', { delay: 300 });
+ScrollReveal().reveal('.image-title', { delay: 550 });
+ScrollReveal().reveal('.quienes-somos', { delay: 300 });
 
 function onMobile () {
     var w = window.innerWidth;
     
     if (w < 666) {
+        console.log('mobile')
         $('nav a').css('font-size', '0.8rem')
         
         $('.image-title').css('width', '20rem')
@@ -13,9 +15,10 @@ function onMobile () {
         $('.image-title').css('transform', 'translateY(2.7rem)')
         $('.title-bg').css('height', '25rem')
     } else {
+        console.log('pc')
         $('nav a').css('font-size', '1.2rem')
 
-        $('.image-title').css('width', '30rem')
+        $('.image-title').css('width', '31rem')
         $('.title-bg span').css('font-size', '4.5rem')
         $('.title-bg span').css('transform', 'translateY(3rem)')
         $('.image-title').css('transform', 'translateY(5.7rem)')
